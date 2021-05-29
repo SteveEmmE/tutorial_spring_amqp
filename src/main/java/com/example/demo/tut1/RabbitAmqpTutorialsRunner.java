9828558d89ef8e1f09ac78be9f274e51b884aaa5
Monti Stefano <stefano@arch-steevux.localdomain>
2021-05-29 14:48:11 +0200

    first commit

---
+++ src/main/java/com/example/demo/tut1/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,11 @@
+package com.example.demo.tut1;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        System.out.println("Ready ... running for " +"ms");
+    }
+}
